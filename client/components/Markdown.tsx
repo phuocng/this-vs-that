75ca297990b3f133fb5dffce06e45fd1abd46d7f
Phuoc Nguyen <me@phuoc.ng>
2020-05-23 23:41:43 +0700

    Use div for paragraph

--- client/components/Markdown.tsx
+++ client/components/Markdown.tsx
@@ -50,7 +50,7 @@ const Markdown: React.FC<MarkdownProps> = ({ content }) => {
                 : <ul className="mb-8 ml-4 list-disc">{p.children}</ul>;
     };
     const renderParagraph = (p: any) => {
-        return <p className="mb-2">{p.children}</p>;
+        return <div className="mb-2">{p.children}</div>;
     };
     const renderTable = (p: any) => {
         return <table className="border border-gray-400 mb-8">{p.children}</table>;
