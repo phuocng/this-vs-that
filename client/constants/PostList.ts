d6c7d089d5d281cbb349f4a3e3abadc5bcaa196f
Phuoc Nguyen <me@phuoc.ng>
2020-06-02 16:05:05 +0700

    argument vs parameter

--- client/constants/PostList.ts
+++ client/constants/PostList.ts
@@ -5,6 +5,7 @@ const PostList: Post[] = [
     { slug: 'add-event-listener-function-vs-on-property', title: 'addEventListener() function vs on property', category: 'JavaScript' },
     { slug: 'alt-vs-title', title: 'alt vs title', category: 'HTML' },
     { slug: 'apply-vs-call', title: 'apply vs call', category: 'JavaScript' },
+    { slug: 'argument-vs-parameter', title: 'argument vs parameter', category: 'JavaScript' },
     { slug: 'class-vs-id', title: 'class vs id', category: 'HTML' },
     { slug: 'display-none-vs-opacity-0-vs-visibility-hidden', title: 'display: none vs opacity: 0 vs visibility: hidden', category: 'CSS' },
     { slug: 'element-vs-node', title: 'Element vs Node', category: 'HTML' },
---
+++ client/posts/argument-vs-parameter/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Markdown from '../../components/Markdown';
+
+export default () => {
+    return (
+<Markdown
+    content={`
+## Difference
+
+A _parameter_ is the variable belonging to the declaration of a function.
+
+An _argument_ is the actual value of the parameter that is passed to the function.
+
+Assume that we have a function that calculate the sum of two numbers:
+
+~~~ javascript
+const sum = function(a, b) {
+    return a + b;
+};
+~~~
+
+here \`a\` and \`b\` are the parameters. If we call the function, \`sum(1, 2)\`, then \`1\` and \`2\` are the arguments.
+
+## Tip
+
+There is a handy memory hook to distinguish these terms:
+
+__P__arameter → __P__laceholder
+
+__A__rgument → __A__ctual value
+`}
+/>
+    );
+};
