85b02dd724860018ba2b4d1cdfac87bb0cfb18df
Azriz Jasni <9023187+AzrizHaziq@users.noreply.github.com>
2020-07-20 21:37:07 +0800

    Fix `thestopImmediatePropagation` to `stopImmediatePropagation`

--- client/posts/stop-immediate-propagation-vs-stop-propagation/index.tsx
+++ client/posts/stop-immediate-propagation-vs-stop-propagation/index.tsx
@@ -12,7 +12,7 @@ Plus, it prevents the other listeners of the same event from being called.
 Let's say that we attach different listeners that handles the same event of the same element. When the event occurs, 
 the listeners are executed in the same order as they're added.
 
-If we call the \`thestopImmediatePropagation()\` method in a given listener, then the remaining listeners will not be called.
+If we call the \`stopImmediatePropagation()\` method in a given listener, then the remaining listeners will not be called.
 
 In the following sample code, there're 3 listeners that handle the click event of a button represented by \`button\`.
 
