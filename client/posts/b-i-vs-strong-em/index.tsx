d9add828a7f62505b8bd702ee5f762208bd013c3
JezDriver <42317898+JezDriver@users.noreply.github.com>
2020-08-07 09:42:17 +0930

    Update '<b>, <i> vs <strong>, <em>' page
    - Fixed 'ans' typo
    - Reworded screenreader sentence for improved grammar

--- client/posts/b-i-vs-strong-em/index.tsx
+++ client/posts/b-i-vs-strong-em/index.tsx
@@ -8,7 +8,7 @@ export default () => {
     content={`
 The \`b\`, \`strong\` tags (and the other pair of tags, \`i\` and \`em\`) look like the same to each other when we see them rendered in a webpage.
 
-Each browser has its own default styles, and it uses the same styles for the \`b\` ans \`strong\` tags.
+Each browser has its own default styles, and it uses the same styles for the \`b\` and \`strong\` tags.
 Here is how they are styled in the popular browsers:
 
 [Chrome](https://chromium.googlesource.com/chromium/blink/+/master/Source/core/css/html.css):
@@ -50,7 +50,7 @@ Without any custom styles, both the \`b\` and \`strong\` tags are bold, while th
 Despite the fact that their appearances are similar, the \`strong\` and \`em\` tags add extra semantic meaning to the enclosed text.
 
 According to the HTML 5 specifications, the \`strong\` and \`em\` tags indicate the importances and emphasis respectively.
-On a screen reader, there's nothing happen when it sees an \`i\` tag. But when it comes across an \`em\` tag, it knows that the content should be 
+On a screen reader, nothing special happens when it sees an \`i\` tag. But when it comes across an \`em\` tag, it knows that the content should be 
 emphasised to the listeners.
 `}
 />
